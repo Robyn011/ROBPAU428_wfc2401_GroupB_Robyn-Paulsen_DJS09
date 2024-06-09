4b7b678c233ce7b44a8b986dcdcd2537e0c9c8f4
Robyn011 <robyncocopumpoen@gmail.com>
2024-06-09 17:00:24 +0200

    fixing website mini challenge

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-// Boolean Types mini-challenge
-// if the last reviewer is a loyalty User, can you add a star to the end of their name?
-// please do so in the existing function, and make sure to declare what type of 
-// parameters the function takes.
-// : boolean
+// Fixing the Website
 
+// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for 
+// our couch surfing website? Do this by assigning types to the parameters
+// we pass to our populateUser function, so that we can be aware of
+// errors in our user objects in the future.
+
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
@@ -27,11 +30,29 @@ const reviews = [
     },
 ]
 
-// Solution
-function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
 
